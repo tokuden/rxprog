3b81bade963ccbe26f019986b7b785f39633ccb8
nahitafu <naitou@tokudenkairo.co.jp>
2012-01-24 11:55:45 +0900

    a

---
+++ src/rxprogwin/mkrelease.bat
@@ -0,0 +1,8 @@
+del /F /Q release\*.*
+copy rxprogwin.exe rxprogwin\
+copy none.bmp      rxprogwin\
+copy np1040.bmp    rxprogwin\
+copy np1041.bmp    rxprogwin\
+copy np1050.bmp    rxprogwin\
+copy license.txt   rxprogwin\
+
